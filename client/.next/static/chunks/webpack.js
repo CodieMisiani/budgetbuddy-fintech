f7b3f90a1f2afb2c95fbcf6410a3e0d7bb84af0c
CodieMisiani <nimrodmisiani1@gmail.com>
2025-07-22 01:13:47 +0300

    chore: update next config

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1341d7788becaad0"; }
+/******/ 		__webpack_require__.h = function() { return "9d361cb198047102"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
