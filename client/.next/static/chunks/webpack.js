8f9a86143004ebc0606efb36da5e98253109b661
CodieMisiani <nimrodmisiani1@gmail.com>
2025-07-21 23:56:13 +0300

    Resolved merge conflicts

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b7779163cbc524b0"; }
+/******/ 		__webpack_require__.h = function() { return "ead8285b13ab5671"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
